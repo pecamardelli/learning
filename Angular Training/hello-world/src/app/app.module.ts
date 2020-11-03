1418806a4cd3401e3baf43dc9eb10c886b743cc5
Pablo Camardelli <pecamardelli@gmail.com>
2020-11-03 01:17:30 -0300

    Add Angular Training projects

---
+++ Angular Training/hello-world/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
+import { CoursesService } from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
+    AuthorsComponent
+  ],
+  imports: [
+BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    CoursesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular Training/hello-world/src/app/authors.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorsService {
+
+  constructor() { }
+
+  getAuthors() {
+    return ['Author1', 'Author2', 'Author3'];
+  }
+}
